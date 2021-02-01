7a90d52fae9e3b4798d9a281e7e1fdc8eae60c34
Almério José Venâncio Pains Soares Pamplona <39603247+almeriopamplona@users.noreply.github.com>
2021-01-31 21:20:59 -0300

    Update and rename mps_align.h to cmps_align.h

--- include/cmps_align.h
+++ include/cmps_align.h
@@ -1,6 +1,6 @@
 /******************************************************************************
  *                   MPS - MOVING PARTICLES SEMI-IMPLICIT                     *
- *                             MPS_ALIGN.h                                    *
+ *                             CMPS_ALIGN.h                                    *
  ******************************************************************************
  * Author: Almério José Venâncio Pains Soares Pamplona                        *
  * E-mail: almeriopamplona@gmail.com                                          *
@@ -11,10 +11,10 @@
  * The full licence is in the file LICENSE, distributed with this software.   *
  ******************************************************************************/
 
-#ifndef __MPS_ALIGN_H__
-#define __MPS_ALIGN_H__
+#ifndef __CMPS_ALIGN_H__
+#define __CMPS_ALIGN_H__
 
-#include "mps_instruction_set.h"
+#include "cmps_instruction_set.h"
 
 /******************************************************************************
  * PLATFORM CHECKS FOR ALIGNED MALLOC FUNCTIONS                               *
@@ -22,26 +22,26 @@
 
 #if ((defined __QNXNTO__) || (defined _GNU_SOURCE) || ((defined _XOPEN_SOURCE) && (_XOPEN_SOURCE >= 600))) \
  && (defined _POSIX_ADVISORY_INFO) && (_POSIX_ADVISORY_INFO > 0)
-  #define MPS_HAS_POSIX_MEMALIGN 1
+  #define CMPS_HAS_POSIX_MEMALIGN 1
 #else
-  #define MPS_HAS_POSIX_MEMALIGN 0
+  #define CMPS_HAS_POSIX_MEMALIGN 0
 #endif
 
-#if defined(MPS_X86_INSTR_SET_AVAILABLE)
-    #define MPS_HAS_MM_MALLOC 1
+#if defined(CMPS_X86_INSTR_SET_AVAILABLE)
+    #define CMPS_HAS_MM_MALLOC 1
 #else
-    #define MPS_HAS_MM_MALLOC 0
+    #define CMPS_HAS_MM_MALLOC 0
 #endif
 
 /******************************************************************************
  * STACK ALLOCATION                                                           *
  ******************************************************************************/
 
-#ifndef MPS_ALLOCA
+#ifndef CMPS_ALLOCA
     #if defined(__linux__)
-        #define MPS_ALLOCA alloca
+        #define CMPS_ALLOCA alloca
     #elif defined(_MSC_VER)
-        #define MPS_ALLOCA _alloca
+        #define CMPS_ALLOCA _alloca
     #endif
 #endif
 
@@ -49,23 +49,23 @@
  * DEFAULT ALIGNMENT                                                          *
  ******************************************************************************/
 
-#if MPS_X86_INSTR_SET >= MPS_X86_AVX512_VERSION
-    #define MPS_DEFAULT_ALIGNMENT 64
-#elif MPS_X86_INSTR_SET >= MPS_X86_AVX_VERSION
-    #define MPS_DEFAULT_ALIGNMENT 32
-#elif MPS_X86_INSTR_SET >= MPS_X86_SSE2_VERSION
-    #define MPS_DEFAULT_ALIGNMENT 16
-#elif MPS_ARM_INSTR_SET >= MPS_ARM8_64_NEON_VERSION
-    #define MPS_DEFAULT_ALIGNMENT 32
-#elif MPS_ARM_INSTR_SET >= MPS_ARM7_NEON_VERSION
-    #define MPS_DEFAULT_ALIGNMENT 16
+#if CMPS_X86_INSTR_SET >= CMPS_X86_AVX512_VERSION
+    #define CMPS_DEFAULT_ALIGNMENT 64
+#elif CMPS_X86_INSTR_SET >= CMPS_X86_AVX_VERSION
+    #define CMPS_DEFAULT_ALIGNMENT 32
+#elif CMPS_X86_INSTR_SET >= CMPS_X86_SSE2_VERSION
+    #define CMPS_DEFAULT_ALIGNMENT 16
+#elif CMPS_ARM_INSTR_SET >= CMPS_ARM8_64_NEON_VERSION
+    #define CMPS_DEFAULT_ALIGNMENT 32
+#elif CMPS_ARM_INSTR_SET >= CMPS_ARM7_NEON_VERSION
+    #define CMPS_DEFAULT_ALIGNMENT 16
 #else
     // some versions of gcc do nos handle alignment set ot 0
     // see https://gcc.gnu.org/bugzilla/show_bug.cgi?id=69089
     #if __GNUC__ < 7
-        #define MPS_DEFAULT_ALIGNMENT 8
+        #define CMPS_DEFAULT_ALIGNMENT 8
     #else
-        #define MPS_DEFAULT_ALIGNMENT 0
+        #define CMPS_DEFAULT_ALIGNMENT 0
     #endif
 #endif
 
